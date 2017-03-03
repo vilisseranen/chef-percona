2e01123816d5d5bc46554bf0e0fae0512abc6879
Eric Rouleau <erouleau@cloud.ca>
2017-03-03 14:33:37 -0500

    fix client attributes

--- recipes/client.rb
+++ recipes/client.rb
@@ -5,6 +5,40 @@
 
 include_recipe "percona::package_repo"
 
+version = value_for_platform_family(
+  "debian" => node["percona"]["version"],
+  "rhel" => node["percona"]["version"].tr(".", "")
+)
+
+case node["platform_family"]
+when "debian"
+  abi_version = case version
+                when "5.5" then "18"
+                when "5.6" then "18.1"
+                else ""
+                end
+
+  if Array(node["percona"]["server"]["role"]).include?("cluster")
+    node.default["percona"]["client"]["packages"] = %W[
+      libperconaserverclient#{abi_version}-dev percona-xtradb-cluster-client-#{version}
+    ]
+  else
+    node.default["percona"]["client"]["packages"] = %W[
+      libperconaserverclient#{abi_version}-dev percona-server-client-#{version}
+    ]
+  end
+when "rhel"
+  if Array(node["percona"]["server"]["role"]).include?("cluster")
+    node.default["percona"]["client"]["packages"] = %W[
+      Percona-XtraDB-Cluster-devel-#{version} Percona-XtraDB-Cluster-client-#{version}
+    ]
+  else
+    node.default["percona"]["client"]["packages"] = %W[
+      Percona-Server-devel-#{version} Percona-Server-client-#{version}
+    ]
+  end
+end
+
 node["percona"]["client"]["packages"].each do |percona_client_pkg|
   package percona_client_pkg do
     action node["percona"]["client"]["package_action"].to_sym

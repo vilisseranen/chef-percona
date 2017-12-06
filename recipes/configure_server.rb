9c1144a07dc843c63f5868d4920243248f1ecfb7
Donatas Abraitis <donatas.abraitis@gmail.com>
2017-12-06 12:23:59 +0200

    Do not initialize mysqld if 5.7 (systemd does it)

--- recipes/configure_server.rb
+++ recipes/configure_server.rb
@@ -108,9 +108,9 @@ end
 
 # install db to the data directory
 execute "setup mysql datadir" do
-  command "#{node["percona"]["version"] == "5.7" ? "mysqld --initialize" : "mysql_install_db"} --defaults-file=#{percona["main_config_file"]} --user=#{user}" # rubocop:disable LineLength
+  command "mysql_install_db --defaults-file=#{percona["main_config_file"]} --user=#{user}" # rubocop:disable LineLength
   not_if "test -f #{datadir}/mysql/user.frm"
-  not_if "test -f #{datadir}/mysql/ibdata1"
+  not_if { percona["version"] == "5.7" }
   action :nothing
 end
 
